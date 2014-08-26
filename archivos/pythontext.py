e19c745bc0186da99a21c240abe1cbd3c68f3cb2
SergioPablo <sp4edelete@gmail.com>
2014-08-25 22:26:30 -0400

    - Actualizado al texto de clases - Arreglado el bug del espacio vacío - Borré archivos innecesarios e hice una idea mejor

--- archivos/pythontext.py
+++ archivos/pythontext.py
@@ -2,7 +2,7 @@ __author__ = 'Sergio'
 # -*- coding: utf-8 -*-
 # Proyecto hecho en python 2.7.x
 archivo_texto = open('texto')
-diccionario_palabras = dict()
+diccionario_palabras = set()
 diccionario_transiciones = dict()
 caracteres_especiales = ['  ' ,' ', '-', '=', '"', '/', '(', ')',';', ':']
 palabra_anterior = '' #Acá declaré la palabra anterior fuera del "for" para que después a medida que pasen las lineas no la borre
@@ -13,10 +13,7 @@ for linea in archivo_texto:
     for palabra_actual in linea.strip().split(' '):
         if palabra_actual != '':
 
-            if palabra_actual not in diccionario_palabras:
-                diccionario_palabras[palabra_actual] = 1
-            else:
-                diccionario_palabras[palabra_actual] += 1
+            #diccionario_palabras.add(palabra_actual)
             if palabra_actual not in diccionario_transiciones:
                 diccionario_transiciones[palabra_actual] = dict() #Verifico que la palabra exista en el dict de transiciones
             if palabra_anterior != '':
@@ -27,21 +24,24 @@ for linea in archivo_texto:
             palabra_anterior = palabra_actual
             palabra_actual = ''
 
-
 archivo_texto.close()
-del archivo_texto
-archivo_datos = open('datos', 'w')
-datos = ''
-for i in diccionario_palabras:
-    datos += (i + ':' + str(diccionario_palabras[i]) + '\n')
-archivo_datos.write(datos)
-del datos
-del archivo_datos
+'''Intento de matriz
+matriz = [[None]]
+for palabra in diccionario_palabras:
+    matriz[0].append(palabra)
+for palabra in range(1, len(matriz[0])):
+    for p in (diccionario_transiciones[matriz[0][palabra]]):
+        print p
+print matriz
+
+
+'''
 archivo_transiciones = open('transiciones','w')
 transiciones = ''
 for palabra in diccionario_transiciones:
-    transiciones += str(palabra) + ':' + str(diccionario_transiciones[palabra]) + '\n'
+    transiciones += str(palabra) + ':' + str((diccionario_transiciones[palabra])) + '\n'
 archivo_transiciones.write(transiciones)
 archivo_transiciones.close()
 del transiciones
 del diccionario_transiciones #Borre variables
+
